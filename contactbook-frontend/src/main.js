dddc6179689771e49ca3fe7c7594cd8a4de1b9c0
nguyenmaixuanhuong <xuanhuong22211@gmail.com>
2023-10-17 15:21:22 +0700

    Hien thi danh sach cac contact lay ve tu server

--- contactbook-frontend/src/main.js
+++ contactbook-frontend/src/main.js
@@ -2,5 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from "./router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+createApp(App).use(router).mount("#app");
 
-createApp(App).mount('#app')
---
+++ contactbook-frontend/src/router/index.js
@@ -0,0 +1,36 @@
+import { createWebHistory, createRouter } from "vue-router";
+import ContactBook from "@/views/ContactBook.vue";
+// import NotFoundView from "@/views/NotFound.vue";
+// import ContactEditView from "@/views/ContactEdit.vue";
+// import ContactAddView from "@/views/ContactAdd.vue";
+const routes = [
+  {
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
+  },
+  
+  // {
+  //   path: "/contacts/:id",
+  //   name: "contact.edit",
+  //   component: ContactEditView,
+  //   props: true 
+  // },
+  // {
+  //   path: "/contacts/add",
+  //   name: "contact.add",
+  //   component: ContactAddView,
+  // },
+  // {
+  //   path: "/:pathMatch(.*)*",
+  //   name: "notfound",
+  //   component: NotFoundView,
+  // },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
